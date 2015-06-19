acb3749c4a044d2f5e927ddcc7a382da4c578219
eroux <elie.roux@telecom-bretagne.eu>
2015-06-19 17:48:35 +0200

    oops

--- sync/syncServer.php
+++ sync/syncServer.php
@@ -125,7 +125,7 @@ function syncWithClient($db, $clientChanges, $minServerVersion, $requests, $role
       $continued = true;
       $maxVersion = max($minServerVersion + 1, floor(($minServerVersion + $maxVersion) / 2));
    }
-   $serverCounts = 0;//syncGetCounts($db, $requests, $minServerVersion, $maxVersion);
+   $serverCounts = syncGetCounts($db, $requests, $minServerVersion, $maxVersion);
    if (function_exists("syncAddCustomServerChanges")) {
       syncDebug('syncAddCustomServerChanges', 'begin');
       syncAddCustomServerChanges($db, $minServerVersion, $serverChanges, $serverCounts, $params);
